00868094ce2b489aa6d730f83794036d17499e1e
gluay88 <p.wantagaew@gmail.com>
2022-02-02 20:20:19 -0800

    updated to describe the actual effect being tested

--- src/pages/CatIndex.test.js
+++ src/pages/CatIndex.test.js
@@ -5,10 +5,17 @@ import CatIndex from "./CatIndex";
 
 Enzyme.configure({adapter: new Adapter()})
 
-describe("When CatIndex renders", () => {
-    it("displays name of cats from mockCats", () => {
-        const catIndex = shallow(<CatIndex />)
-        const allcats = catIndex.find("p").text()
-        expect(allcats).toEqual("No cats")
+// describe("When CatIndex renders", () => {
+//     it("displays names of cats from mockCats", () => {
+//         const catIndex = shallow(<CatIndex />)
+//         const allcats = catIndex.find("p").text()
+//         expect(allcats).toEqual("No cats")
+//     })
+
+    describe("When CatIndex renders", () => {
+        it("displays all names of cats from cats", () => {
+            const catIndex = shallow(<CatIndex />)
+            const allcats = catIndex.find("p").text()
+            expect(allcats).toEqual("No cats")
     })
 })
